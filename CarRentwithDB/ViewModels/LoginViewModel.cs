36be13ddafe0f857f48a903542bd957f1cb165a6
DanielWajd <wajdowiczdaniel@gmail.com>
2024-10-21 17:07:36 +0200

    Added first login

---
+++ CarRentwithDB/ViewModels/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarRentwithDB.ViewModels
+{
+    public class LoginViewModel
+    {
+        //[Display(Name = "Email Address")]
+        //[Required(ErrorMessage = "Adres email jest wymagany")]
+        public string EmailAddress { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
